49000dda3851dd9af2ae40a7c232407821ed26eb
Maxim Brishten <b3tmanann@gmail.com>
2019-07-07 14:06:42 +0300

    init project

---
+++ AudiOBD2Scanner/AudiOBD2Scanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AudiOBD2Scanner
+//
+//  Created by Макс on 7/7/19.
+//  Copyright © 2019 b3tman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
