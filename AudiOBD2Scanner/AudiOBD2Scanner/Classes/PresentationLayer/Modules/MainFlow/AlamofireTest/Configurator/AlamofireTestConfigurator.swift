8f879ed217de181e131bbff3c5dcc32995246f60
b3tman <b3tmanann@gmail.com>
2021-11-19 17:41:45 +0300

    Added AlamofireTest module

---
+++ AudiOBD2Scanner/AudiOBD2Scanner/Classes/PresentationLayer/Modules/MainFlow/AlamofireTest/Configurator/AlamofireTestConfigurator.swift
@@ -0,0 +1,20 @@
+//
+//  AlamofireTestConfigurator.swift
+//  AudiOBD2Scanner
+//
+//  Created Maxim Brishten on 19.11.21.
+//  Copyright © 2021 b3tman. All rights reserved.
+//
+
+import UIKit
+import Swinject
+
+final class AlamofireTestModuleConfigurator {
+    
+    func configureModule () -> AlamofireTestModuleInput {
+        
+        let moduleInput = AppDelegate.moduleAssembly.resolver.resolve(AlamofireTestModuleInput.self)!
+        
+        return moduleInput
+    }
+}
